a59bb56ae74878428788b6ad242da14d8a3e0118
Himanshu-4444 <154578231+Himanshu-4444@users.noreply.github.com>
2024-01-08 23:03:17 +0530

    Update info.py

--- info.py
+++ info.py
@@ -120,9 +120,9 @@ else:
     ON_HEROKU = False
 HAS_SSL=bool(getenv('HAS_SSL',False))
 if HAS_SSL:
-    URL = "https://{}/".format(FQDN)
+    URL = "https://{FQDN}/".format(FQDN)
 else:
-    URL = "https://{}/".format(FQDN)
+    URL = "http://{FQDN}/".format(FQDN)
 
 
 
